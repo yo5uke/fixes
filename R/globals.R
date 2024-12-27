72e313ad57f6f4914e42a219e29742c82034f86d
yo5uke <yosuke.abe0507@gmail.com>
2024-12-27 20:27:17 +0900

    Add new variable

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,3 @@
 utils::globalVariables(c(
-  "relative_time", "estimate", "conf_low", "conf_high", "term", "std.error", ":="
+  "relative_time", "estimate", "conf_low", "conf_high", "term", "std.error", ":=", "is_baseline"
 ))
